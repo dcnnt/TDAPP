a3a70bc91682a9b432e620b876f246549d1fe4ae
dcannt <daniel.canton.eng@gmail.com>
2026-09-03 14:50:26 +0200

    Initial Commit

---
+++ Tracker-DAH/Tracker_DAHApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tracker_DAHApp.swift
+//  Tracker-DAH
+//
+//  Created by Dan on 03/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct Tracker_DAHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Tracker-DAHTests/Tracker_DAHTests.swift
@@ -0,0 +1,19 @@
+//
+//  Tracker_DAHTests.swift
+//  Tracker-DAHTests
+//
+//  Created by Dan on 03/09/2026.
+//
+
+import Testing
+@testable import Tracker_DAH
+
+struct Tracker_DAHTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
